a3a40b08c697547501093bab823f4cb5ae87081c
wmss <wmss.wj.true@gmail.com>
2016-06-24 22:52:25 +0800

    开始写入第二章的第一个控件

--- app/src/main/java/tk/wmss/firstlinecode/secondchapter/SecondChapterActivity.java
+++ app/src/main/java/tk/wmss/firstlinecode/secondchapter/SecondChapterActivity.java
@@ -2,6 +2,7 @@ package tk.wmss.firstlinecode.secondchapter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import tk.wmss.firstlinecode.R;
 
@@ -9,10 +10,20 @@ import tk.wmss.firstlinecode.R;
  * Created by wmss on 2016/6/24.
  */
 public class SecondChapterActivity extends AppCompatActivity {
+
+    private boolean B = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_chapter);
+    }
 
+    public void hideTitle(View view) {
+        if (B) {
+            B = false;
+            getSupportActionBar().hide();
+        } else {
+            B = true;
+        }
     }
 }
