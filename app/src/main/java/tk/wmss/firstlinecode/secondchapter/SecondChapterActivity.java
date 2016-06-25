6723125cafb4e64fbb548c07583fd22a1f100818
wmss <wmss.wj.true@gmail.com>
2016-06-25 14:30:44 +0800

    开始写入第二章的菜单实现

--- app/src/main/java/tk/wmss/firstlinecode/secondchapter/SecondChapterActivity.java
+++ app/src/main/java/tk/wmss/firstlinecode/secondchapter/SecondChapterActivity.java
@@ -2,7 +2,10 @@ package tk.wmss.firstlinecode.secondchapter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import tk.wmss.firstlinecode.R;
 
@@ -27,5 +30,31 @@ public class SecondChapterActivity extends AppCompatActivity {
             getSupportActionBar().show();
             B = true;
         }
+    }
+
+    public void showToast(View view) {
+        /*
+        * Toast.makeText(上下文,显示的字符串,显示的时间).show;
+        * */
+        Toast.makeText(SecondChapterActivity.this, "我就是那个要显示的Toast", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.second_chapter, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(SecondChapterActivity.this, "you clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(SecondChapterActivity.this, "you clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
