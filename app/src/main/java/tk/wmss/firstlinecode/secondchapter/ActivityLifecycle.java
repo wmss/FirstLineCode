f4b0880efdb72f9d9ddb5479f1b84e1d3141a1e6
wmss <wmss.wj.true@gmail.com>
2016-06-29 20:12:22 +0800

    更新代码样式，更改错误的布局文件名

--- app/src/main/java/tk/wmss/firstlinecode/secondchapter/ActivityLifecycle.java
+++ app/src/main/java/tk/wmss/firstlinecode/secondchapter/ActivityLifecycle.java
@@ -17,104 +17,105 @@ import tk.wmss.firstlinecode.TestGoToActivity;
  * Created by wmss on 2016/6/28.
  */
 public class ActivityLifecycle extends AppCompatActivity {
-    private static final String TAG = "在ActivityLifecycle下调用了";
+	private static final String TAG = "在ActivityLifecycle下调用了";
 
-    @SuppressWarnings("ConstantConditions")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate");
-        setContentView(R.layout.activity_lfecycle);
-        getSupportActionBar().hide();
-    }
+	@SuppressWarnings("ConstantConditions")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.i(TAG, "onCreate");
+		setContentView(R.layout.activity_lifecycle);
+		getSupportActionBar().hide();
+	}
 
-    @Override
-    public void onContentChanged() {
-        super.onContentChanged();
-        Log.i(TAG, "onContentChanged");
-    }
+	@Override
+	public void onContentChanged() {
+		super.onContentChanged();
+		Log.i(TAG, "onContentChanged");
+	}
 
-    public void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart");
-    }
+	public void onStart() {
+		super.onStart();
+		Log.i(TAG, "onStart");
+	}
 
-    public void onRestart() {
-        super.onRestart();
-        Log.i(TAG, "onRestart");
-    }
+	public void onRestart() {
+		super.onRestart();
+		Log.i(TAG, "onRestart");
+	}
 
-    public void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        Log.i(TAG, "onPostCreate");
-    }
+	public void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		Log.i(TAG, "onPostCreate");
+	}
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume");
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.i(TAG, "onResume");
+	}
 
-    public void onPostResume() {
-        super.onPostResume();
-        Log.i(TAG, "onPostResume");
-    }
+	public void onPostResume() {
+		super.onPostResume();
+		Log.i(TAG, "onPostResume");
+	}
 
-    public void onPause() {
-        super.onPause();
-        Log.i(TAG, "onPause");
-    }
+	public void onPause() {
+		super.onPause();
+		Log.i(TAG, "onPause");
+	}
 
-    public void onStop() {
-        super.onStop();
-        Log.i(TAG, "onStop");
-    }
+	public void onStop() {
+		super.onStop();
+		Log.i(TAG, "onStop");
+	}
 
-    public void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy");
-    }
+	public void onDestroy() {
+		super.onDestroy();
+		Log.i(TAG, "onDestroy");
+	}
 
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        Log.i(TAG, "onConfigurationChanged");
-    }
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		Log.i(TAG, "onConfigurationChanged");
+	}
 
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Log.i(TAG, "onSaveInstanceState");
-    }
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Log.i(TAG, "onSaveInstanceState");
+	}
 
-    public void onRestoreInstanceState(Bundle outState) {
-        super.onRestoreInstanceState(outState);
-        Log.i(TAG, "onRestoreInstanceState");
-    }
+	public void onRestoreInstanceState(Bundle outState) {
+		super.onRestoreInstanceState(outState);
+		Log.i(TAG, "onRestoreInstanceState");
+	}
 
-    public void goToTest(View view) {
-        Intent intent = new Intent(this, TestGoToActivity.class);
-        startActivity(intent);
-    }
+	public void goToTest(View view) {
+		Intent intent = new Intent(this, TestGoToActivity.class);
+		startActivity(intent);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_lifecycle_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_lifecycle_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.activity_lifecycle_menu_item1:
-                Toast.makeText(ActivityLifecycle.this, "我是一个消息", Toast.LENGTH_SHORT).show();
-                break;
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.activity_lifecycle_menu_item1:
+				Toast.makeText(ActivityLifecycle.this, "我是一个消息", Toast.LENGTH_SHORT).show();
+				break;
 
-            case R.id.activity_lifecycle_menu_item2:
-                Intent intent = new Intent(this, TestActivityLifecycle.class);
-                startActivity(intent);
-                break;
+			case R.id.activity_lifecycle_menu_item2:
+				Intent intent = new Intent(this, TestActivityLifecycle.class);
+				startActivity(intent);
+				break;
 
-            default:
-        }
-        return true;
-    }
+			default:
+		}
+		return true;
+	}
+
 }
