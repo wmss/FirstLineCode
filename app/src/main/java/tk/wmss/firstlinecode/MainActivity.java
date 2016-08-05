d6b0a063fdf8c7c93c185a88947c2322f9cbe79e
wmss <wmss.wj.true@gmail.com>
2016-08-05 23:27:36 +0800

    添加了封面将会自动关闭（跳转）的code

--- app/src/main/java/tk/wmss/firstlinecode/MainActivity.java
+++ app/src/main/java/tk/wmss/firstlinecode/MainActivity.java
@@ -5,19 +5,36 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-    }
+	private Intent intent = new Intent(this, DirectoryActivity.class);//你要转向的Activity
 
-    public void goToDirectory(View view) {
-        Intent intent = new Intent(this, DirectoryActivity.class);
-        startActivity(intent);
-        finish();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		getSupportActionBar().hide();
+
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				goToDirectory();
+			}
+		};
+		timer.schedule(task, 1000 * 3);
+	}
+
+	public void goToDirectory(View view) {
+		goToDirectory();
+	}
+
+	private void goToDirectory() {
+		startActivity(intent);
+		finish();
+	}
 
 }
